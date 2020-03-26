55d1ca3c80d37c7007e693c1882b87fa7da202c8
Børhaug <Brede.Borhaug@dnvgl.com>
2020-03-26 21:38:51 +0100

    Adding tooling to remote repo

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonwebtoken = require(`jsonwebtoken`)
+
+
+
+function authenticateToken(bearertoken, publickey, signOptions) {
+    if ( typeof jwt == `undefined`) throw new Error('jwt is undefined')
+    if ( typeof publicKey == `undefined`) throw new Error('publickey is undefined')
+    const jwt = bearertoken.split(' ') [1]
+    jsonwebtoken.verify(jwt, publickey, signOptions, (err, authData) => {
+        if(err) {
+            throw new Error('token not valid')
+        } 
+        return authData   
+    });
+}
+
+module.exports = authenticateToken;
